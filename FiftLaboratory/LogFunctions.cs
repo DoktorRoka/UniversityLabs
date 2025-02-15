283ccfee29f06bf8d6d01ba62ade26e232bd88c3
Maxim <137930299+DoktorRoka@users.noreply.github.com>
2025-02-15 21:14:19 +0300

    Пятая лабораторная

---
+++ FiftLaboratory/LogFunctions.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace FifthLaboratoryIVT7
+{
+    public class LogFunctions
+    {
+        public void ViewLog(List<LogEntry> log, DateTime lastActionTime)
+        {
+            foreach (var entry in log)
+            {
+                Console.WriteLine(entry);
+            }
+
+            Console.WriteLine("\nСамый долгий период бездействия пользователя:");
+            Console.WriteLine((DateTime.Now - lastActionTime).ToString("hh\\:mm\\:ss"));
+        }
+
+        public void LogAction(List<LogEntry> log, string action, string recordInfo, ref DateTime lastActionTime)
+        {
+            log.Add(new LogEntry
+            {
+                Action = action,
+                RecordInfo = recordInfo,
+                Timestamp = DateTime.Now
+            });
+
+            if (log.Count > 50) log.RemoveAt(0);
+            lastActionTime = DateTime.Now;
+        }
+
+        public List<LogEntry> LoadLog(string filePath)
+        {
+            var log = new List<LogEntry>();
+            if (File.Exists(filePath))
+            {
+                var lines = File.ReadAllLines(filePath);
+                foreach (var line in lines)
+                {
+                    var parts = line.Split(' ', 3);
+                    log.Add(new LogEntry
+                    {
+                        Timestamp = DateTime.Parse(parts[0]),
+                        Action = parts[2].Split(' ')[0],
+                        RecordInfo = parts[2].Split('"')[1]
+                    });
+                }
+            }
+            return log;
+        }
+
+        public void SaveLog(List<LogEntry> log, string filePath)
+        {
+            File.WriteAllLines(filePath, log.Select(l => l.ToString()));
+        }
+    }
+}
---
+++ FiftLaboratory/TableFunctions.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace FifthLaboratoryIVT7
+{
+    public class TableFunctions
+    {
+        public void ViewTable(List<Participant> participants)
+        {
+            Console.WriteLine("\nСостав спортклуба:");
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("| ФИО            | Тип | Год рождения | Опыт (лет) |");
+            Console.WriteLine("-------------------------------------------------");
+
+            foreach (var participant in participants)
+            {
+                Console.WriteLine($"| {participant.FullName,-14} | {participant.Type,-3} | {participant.BirthYear,-12} | {participant.Experience,-10} |");
+            }
+
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("Перечисляемый тип: Т - тренер, С - спортсмен");
+        }
+
+        public void AddRecord(List<Participant> participants, List<LogEntry> log, ref DateTime lastActionTime, LogFunctions logFunctions)
+        {
+            Participant participant = new Participant();
+
+            Console.Write("Введите ФИО: ");
+            participant.FullName = Console.ReadLine();
+
+            Console.Write("Введите тип (Т - тренер, С - спортсмен): ");
+            participant.Type = char.ToUpper(Console.ReadLine()[0]);
+
+            Console.Write("Введите год рождения: ");
+            participant.BirthYear = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите опыт (лет): ");
+            participant.Experience = int.Parse(Console.ReadLine());
+
+            participants.Add(participant);
+            logFunctions.LogAction(log, "Добавлена запись", participant.FullName, ref lastActionTime);
+        }
+
+        public void DeleteRecord(List<Participant> participants, List<LogEntry> log, ref DateTime lastActionTime, LogFunctions logFunctions)
+        {
+            Console.Write("Введите номер записи для удаления: ");
+            if (int.TryParse(Console.ReadLine(), out int index) && index > 0 && index <= participants.Count)
+            {
+                var removed = participants[index - 1];
+                participants.RemoveAt(index - 1);
+                logFunctions.LogAction(log, "Удалена запись", removed.FullName, ref lastActionTime);
+            }
+            else
+            {
+                Console.WriteLine("Некорректный номер записи.");
+            }
+        }
+
+        public void UpdateRecord(List<Participant> participants, List<LogEntry> log, ref DateTime lastActionTime, LogFunctions logFunctions)
+        {
+            Console.Write("Введите номер записи для обновления: ");
+            if (int.TryParse(Console.ReadLine(), out int index) && index > 0 && index <= participants.Count)
+            {
+                Participant participant = participants[index - 1];
+
+                Console.Write("Введите новое ФИО: ");
+                participant.FullName = Console.ReadLine();
+
+                Console.Write("Введите новый тип (Т - тренер, С - спортсмен): ");
+                participant.Type = char.ToUpper(Console.ReadLine()[0]);
+
+                Console.Write("Введите новый год рождения: ");
+                participant.BirthYear = int.Parse(Console.ReadLine());
+
+                Console.Write("Введите новый опыт (лет): ");
+                participant.Experience = int.Parse(Console.ReadLine());
+
+                participants[index - 1] = participant;
+                logFunctions.LogAction(log, "Обновлена запись", participant.FullName, ref lastActionTime);
+            }
+            else
+            {
+                Console.WriteLine("Некорректный номер записи.");
+            }
+        }
+
+        public void SearchRecords(List<Participant> participants)
+        {
+            Console.Write("Введите минимальный год рождения для поиска: ");
+            int year = int.Parse(Console.ReadLine());
+
+            var results = participants.Where(p => p.BirthYear > year).ToList();
+
+            if (results.Any())
+            {
+                ViewTable(results);
+            }
+            else
+            {
+                Console.WriteLine("Нет записей, соответствующих критерию.");
+            }
+        }
+
+        public List<Participant> LoadParticipants(string filePath)
+        {
+            var participants = new List<Participant>();
+            if (File.Exists(filePath))
+            {
+                var lines = File.ReadAllLines(filePath);
+                foreach (var line in lines)
+                {
+                    participants.Add(Participant.FromString(line));
+                }
+            }
+            return participants;
+        }
+
+        public void SaveParticipants(List<Participant> participants, string filePath)
+        {
+            File.WriteAllLines(filePath, participants.Select(p => p.ToString()));
+        }
+    }
+}
