c69463fe4f8432f7bd071d05fa0cfb73369bc122
Maxim <137930299+DoktorRoka@users.noreply.github.com>
2026-02-02 17:22:10 +0300

    Add files via upload

---
+++ Laba3_grafika/Laba3/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.IO;
+
+namespace Laba3
+{
+    public partial class Form1 : Form
+    {
+        // Объявляем объект "g" класса Graphics
+        Graphics g;
+        string filename = @"Strings.txt";
+        string[] sm = {
+
+        "First line", "Second line", "Third line",
+        "Fourth line", "Fifth line", "Sixth line",
+        "Seventh line", "Eighth line", "Ninth line",
+        "Tenth line"};
+
+        public Form1()
+        {
+            InitializeComponent();
+            g = pictureBox1.CreateGraphics();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamWriter f = new StreamWriter(new FileStream(filename,
+            FileMode.Create, FileAccess.Write));
+
+            foreach (string s in sm) { f.WriteLine(s); }
+            f.Close();
+            MessageBox.Show("10 строк записано в файл !");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.FromArgb(255, 255, 255));
+            File.Delete(filename);
+            MessageBox.Show("Файл Strings.txt удалён !");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int k = 0;
+            // Чтение строк из файла
+            try
+            {
+                StreamReader f = new StreamReader(new FileStream(filename,
+                FileMode.Open, FileAccess.Read));
+                for (int i = 0; i < 10; i++) { sm[i] = f.ReadLine(); }
+                f.Close();
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+            // **** Отображение строк разными шрифтами и выравниванием **
+            pictureBox1.BackColor = Color.FromName("Azure");
+            pictureBox1.Refresh();
+            for (int i = 0; i < 10; i++)
+            {
+                // Отображение первой группы строк
+                if ((i >= 0) && (i <= 5))
+                {
+                    Font fn = new Font("Arial", 36, FontStyle.Underline);
+
+                    StringFormat sf =
+                    (StringFormat)StringFormat.GenericTypographic.Clone();
+                    sf.FormatFlags = StringFormatFlags.DirectionVertical;
+
+                    sf.Alignment = StringAlignment.Center;
+                    sf.LineAlignment = StringAlignment.Center;
+                    g.DrawString(sm[i], fn, Brushes.Blue,
+                    new RectangleF(0 + i * 50, 0, pictureBox1.Size.Width - 20,
+                    pictureBox1.Size.Height - 20), sf);
+                    fn.Dispose();
+                }
+                // Отображение второй группы строк
+                if ((i >= 6) && (i <= 8))
+                {
+                    k = i - 9;
+                    Font fn = new Font("Broadway", 24, FontStyle.Regular);
+                    StringFormat sf =
+                    (StringFormat)StringFormat.GenericTypographic.Clone();
+                    sf.Alignment = StringAlignment.Far;
+                    sf.LineAlignment = StringAlignment.Near;
+                    g.DrawString(sm[i], fn, Brushes.Black,
+                    new RectangleF(0, 0 + k * -24, pictureBox1.Size.Width - 20,
+                    pictureBox1.Size.Height - 20), sf);
+                    fn.Dispose();
+                }
+                //Отображение третьей группы строк
+                if (i == 9)
+                {
+                    Font fn = new Font("Times New Roman", 36, FontStyle.Strikeout);
+                    StringFormat sf =
+                    (StringFormat)StringFormat.GenericTypographic.Clone();
+                    sf.Alignment = StringAlignment.Near;
+
+                    sf.LineAlignment = StringAlignment.Far;
+                    g.DrawString(sm[i], fn, Brushes.Green,
+                    new RectangleF(0 + i * 5, 0 + i * -5, pictureBox1.Size.Width - 20,
+                    pictureBox1.Size.Height - 20), sf);
+                    fn.Dispose();
+                }
+            }
+        }
+    }
+}
---
+++ Laba5_grafika/Laba5_grafika/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laba5_grafika
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+
+            InitializeComponent();
+
+            using (var g = this.CreateGraphics())
+            {
+                g.DrawString("Кликните мышкой по элементу PictureBox",
+                    new Font("Arial", 10, FontStyle.Regular),
+                    Brushes.Red, 5, 5);
+            }
+
+            pictureBox1.BackColor = Color.WhiteSmoke;
+
+            pictureBox1.Click += pictureBox1_Click;
+        }
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Refresh();
+
+            using (Graphics g = pictureBox1.CreateGraphics())
+            {
+                g.SmoothingMode = SmoothingMode.AntiAlias;
+                g.TextRenderingHint = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+
+                Point[] poly =
+                {
+                    new Point(120, 40), new Point(160, 20), new Point(210, 30),
+                    new Point(260, 20), new Point(310, 30), new Point(360, 20),
+                    new Point(410, 30), new Point(430, 70), new Point(380, 85),
+                    new Point(200, 85), new Point(120, 70)
+                };
+                using (var fill = new HatchBrush(HatchStyle.Wave, Color.MediumPurple, Color.White))
+                using (var pen = new Pen(Color.MediumBlue, 2))
+                { g.FillPolygon(fill, poly); g.DrawPolygon(pen, poly); }
+
+                var title = "Оценки по программированию\nдевяти студентов группы";
+                using (var fn = new Font("Arial", 10, FontStyle.Bold))
+                {
+                    var sf = new StringFormat { Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Center };
+                    g.DrawString(title, fn, Brushes.Crimson, new Rectangle(125, 25, 300, 60), sf);
+                }
+
+                // Рамка
+                g.DrawRectangle(Pens.MidnightBlue, 0, 0, pictureBox1.Width - 1, pictureBox1.Height - 1);
+
+                string[] students = { "Ст1", "Ст2", "Ст3", "Ст4", "Ст5", "Ст6", "Ст7", "Ст8", "Ст9" };
+                int[] grades = { 5, 4, 3, 5, 4, 4, 5, 3, 4 }; // ← замените на свои
+
+                // Поля области построения
+                int left = 55, right = pictureBox1.Width - 20;
+                int top = 120, bottom = pictureBox1.Height - 40;
+
+                // Оси (нулевой уровень – ось X)
+                g.DrawLine(Pens.Black, left, bottom, right, bottom);
+                g.DrawLine(Pens.Black, left, top, left, bottom);
+
+                // Масштаб по пятибалльной шкале
+                int max = Math.Max(5, grades.Max()); // максимум минимум 5
+                float k = (bottom - top) / (float)max; // px на 1 балл
+
+                // Сетка и подписи по Y (0..5)
+                using (var gridPen = new Pen(Color.RoyalBlue, 1.6f) { DashStyle = DashStyle.Dash })
+                using (var yFont = new Font("Arial", 8, FontStyle.Bold))
+                {
+                    var sfY = new StringFormat { Alignment = StringAlignment.Near, LineAlignment = StringAlignment.Center };
+                    for (int v = 0; v <= max; v++)
+                    {
+                        float y = bottom - v * k;
+                        g.DrawLine(gridPen, left - 5, y, right, y);
+                        g.DrawString(v.ToString(), yFont, Brushes.DarkBlue,
+                                     new RectangleF(2, y - 9, left - 8, 18), sfY);
+                    }
+                }
+
+                // Геометрия столбцов
+                int n = grades.Length;
+                int gap = 10;
+                int plotWidth = (right - left);
+                int barWidth = (plotWidth - (n + 1) * gap) / n;
+                int x = left + gap;
+
+                // Кисти
+                using (var solid = new SolidBrush(Color.CornflowerBlue))
+                using (var hatch = new HatchBrush(HatchStyle.BackwardDiagonal, Color.CornflowerBlue, Color.LightSkyBlue))
+                using (var img = Image.FromFile("img.bmp"))
+            
+                using (var texture = new TextureBrush(img))
+                using (var barPen = new Pen(Color.Black, 1))
+                using (var xFont = new Font("Arial", 8, FontStyle.Bold))
+                using (var valFont = new Font("Arial", 8, FontStyle.Regular))
+
+                {
+                    var sfX = new StringFormat { Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Near };
+
+                    for (int i = 0; i < n; i++)
+                    {
+                        int h = (int)Math.Round(grades[i] * k);
+                        var rect = new Rectangle(x, bottom - h, barWidth, h);
+
+                        if (i < 3) g.FillRectangle(solid, rect);
+                        else if (i < 6) g.FillRectangle(hatch, rect);
+                        else g.FillRectangle(texture, rect);
+
+                        g.DrawRectangle(barPen, rect);
+
+                        // метка студента
+                        int tickX = x + barWidth / 2;
+                        g.DrawLine(Pens.Black, tickX, bottom - 5, tickX, bottom + 5);
+                        g.DrawString(students[i], xFont, Brushes.Black,
+                                     new Rectangle(tickX - 25, bottom + 6, 50, 18), sfX);
+
+                        // подпись значения над столбцом
+                        g.DrawString(grades[i].ToString(), valFont, Brushes.Black,
+                                     new PointF(tickX - 6, bottom - h - 14));
+
+                        x += barWidth + gap;
+                    }
+                }
+            }
+        }
+        private static Bitmap MakeTextureTile()
+        {
+            var bmp = new Bitmap(8, 8);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.Clear(Color.MidnightBlue);
+                using (var b = new SolidBrush(Color.SteelBlue))
+                { g.FillRectangle(b, 0, 0, 4, 4); g.FillRectangle(b, 4, 4, 4, 4); }
+            }
+            return bmp;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
