1dd1eacba08cbd6fea849da658b437ccc290cebf
Maxim <137930299+DoktorRoka@users.noreply.github.com>
2026-03-24 21:36:40 +0300

    Add files via upload

---
+++ GrafikaLab3/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace GrafikaLab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.b_to_scratch = new System.Windows.Forms.Button();
+            this.b_read_from_file = new System.Windows.Forms.Button();
+            this.b_clear = new System.Windows.Forms.Button();
+            this.b_exit = new System.Windows.Forms.Button();
+            this.b_change_output_size = new System.Windows.Forms.Button();
+            this.tb_x1 = new System.Windows.Forms.TextBox();
+            this.tb_y1 = new System.Windows.Forms.TextBox();
+            this.tb_y2 = new System.Windows.Forms.TextBox();
+            this.tb_x2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(531, 355);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // b_to_scratch
+            // 
+            this.b_to_scratch.Location = new System.Drawing.Point(12, 382);
+            this.b_to_scratch.Name = "b_to_scratch";
+            this.b_to_scratch.Size = new System.Drawing.Size(152, 23);
+            this.b_to_scratch.TabIndex = 1;
+            this.b_to_scratch.Text = "Записать в файл в скретч";
+            this.b_to_scratch.UseVisualStyleBackColor = true;
+            this.b_to_scratch.Click += new System.EventHandler(this.b_to_scratch_Click);
+            // 
+            // b_read_from_file
+            // 
+            this.b_read_from_file.Location = new System.Drawing.Point(170, 382);
+            this.b_read_from_file.Name = "b_read_from_file";
+            this.b_read_from_file.Size = new System.Drawing.Size(198, 23);
+            this.b_read_from_file.TabIndex = 2;
+            this.b_read_from_file.Text = "Прочитать из файла и отобразить";
+            this.b_read_from_file.UseVisualStyleBackColor = true;
+            this.b_read_from_file.Click += new System.EventHandler(this.b_read_from_file_Click);
+            // 
+            // b_clear
+            // 
+            this.b_clear.Location = new System.Drawing.Point(374, 382);
+            this.b_clear.Name = "b_clear";
+            this.b_clear.Size = new System.Drawing.Size(152, 23);
+            this.b_clear.TabIndex = 3;
+            this.b_clear.Text = "Очистить";
+            this.b_clear.UseVisualStyleBackColor = true;
+            this.b_clear.Click += new System.EventHandler(this.b_clear_Click);
+            // 
+            // b_exit
+            // 
+            this.b_exit.Location = new System.Drawing.Point(636, 415);
+            this.b_exit.Name = "b_exit";
+            this.b_exit.Size = new System.Drawing.Size(152, 23);
+            this.b_exit.TabIndex = 5;
+            this.b_exit.Text = "Выход";
+            this.b_exit.UseVisualStyleBackColor = true;
+            this.b_exit.Click += new System.EventHandler(this.b_exit_Click);
+            // 
+            // b_change_output_size
+            // 
+            this.b_change_output_size.Location = new System.Drawing.Point(598, 187);
+            this.b_change_output_size.Name = "b_change_output_size";
+            this.b_change_output_size.Size = new System.Drawing.Size(152, 23);
+            this.b_change_output_size.TabIndex = 6;
+            this.b_change_output_size.Text = "Изменить область вывода";
+            this.b_change_output_size.UseVisualStyleBackColor = true;
+            this.b_change_output_size.Click += new System.EventHandler(this.b_change_output_size_Click);
+            // 
+            // tb_x1
+            // 
+            this.tb_x1.Location = new System.Drawing.Point(598, 80);
+            this.tb_x1.Name = "tb_x1";
+            this.tb_x1.Size = new System.Drawing.Size(35, 20);
+            this.tb_x1.TabIndex = 7;
+            // 
+            // tb_y1
+            // 
+            this.tb_y1.Location = new System.Drawing.Point(655, 80);
+            this.tb_y1.Name = "tb_y1";
+            this.tb_y1.Size = new System.Drawing.Size(35, 20);
+            this.tb_y1.TabIndex = 8;
+            // 
+            // tb_y2
+            // 
+            this.tb_y2.Location = new System.Drawing.Point(655, 124);
+            this.tb_y2.Name = "tb_y2";
+            this.tb_y2.Size = new System.Drawing.Size(35, 20);
+            this.tb_y2.TabIndex = 10;
+            // 
+            // tb_x2
+            // 
+            this.tb_x2.Location = new System.Drawing.Point(598, 124);
+            this.tb_x2.Name = "tb_x2";
+            this.tb_x2.Size = new System.Drawing.Size(35, 20);
+            this.tb_x2.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(598, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "X1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(652, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Y1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(652, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Y2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(598, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "X2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_y2);
+            this.Controls.Add(this.tb_x2);
+            this.Controls.Add(this.tb_y1);
+            this.Controls.Add(this.tb_x1);
+            this.Controls.Add(this.b_change_output_size);
+            this.Controls.Add(this.b_exit);
+            this.Controls.Add(this.b_clear);
+            this.Controls.Add(this.b_read_from_file);
+            this.Controls.Add(this.b_to_scratch);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Лабораторная работа 11";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button b_to_scratch;
+        private System.Windows.Forms.Button b_read_from_file;
+        private System.Windows.Forms.Button b_clear;
+        private System.Windows.Forms.Button b_exit;
+        private System.Windows.Forms.Button b_change_output_size;
+        private System.Windows.Forms.TextBox tb_x1;
+        private System.Windows.Forms.TextBox tb_y1;
+        private System.Windows.Forms.TextBox tb_y2;
+        private System.Windows.Forms.TextBox tb_x2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ GrafikaLab3/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GrafikaLab3
+{
+    public partial class Form1 : Form
+    {
+
+        public struct Simple
+        {
+            public double xx;
+            public double yy;
+            public int ii; // 0 - move (поднять перо), 1 - draw (опустить перо)
+        };
+
+        // Глобальные переменные для масштабирования (для кнопки чтения файла)
+        double Xmin = 0.2, Xmax = 8.2, Ymin = 0.5, Ymax = 6.5;
+
+        private void b_to_scratch_Click(object sender, EventArgs e)
+        {
+            string fileName = "SCRATCH";
+
+            // Открываем файл для бинарной записи
+            using (BinaryWriter fw = new BinaryWriter(File.Open(fileName, FileMode.Create)))
+            {
+                int n = 6;                // Количество углов (шестиугольник)
+                int iterations = 20;      // 20 вложенных фигур
+                double p = 0.1;           // Коэффициент сдвига (10% от длины грани)
+                double R = 100.0;         // Начальный радиус
+
+                double[] x = new double[n];
+                double[] y = new double[n];
+
+                // Вычисляем вершины самого первого (внешнего) шестиугольника
+                for (int i = 0; i < n; i++)
+                {
+                    double angle = i * (2.0 * Math.PI / n); // Угол 60 градусов
+                    x[i] = R * Math.Cos(angle);
+                    y[i] = R * Math.Sin(angle);
+                }
+
+                Simple s;
+
+                // Цикл генерации 20 вложенных фигур
+                for (int iter = 0; iter < iterations; iter++)
+                {
+                    // === Отрисовка текущего шестиугольника ===
+
+                    // Перемещаем перо в первую точку (code = 0)
+                    s.xx = x[0]; s.yy = y[0]; s.ii = 0;
+                    fw.Write(s.xx); fw.Write(s.yy); fw.Write(s.ii);
+
+                    // Рисуем линии ко всем остальным вершинам и замыкаем фигуру (code = 1)
+                    for (int i = 1; i <= n; i++)
+                    {
+                        s.xx = x[i % n]; s.yy = y[i % n]; s.ii = 1;
+                        fw.Write(s.xx); fw.Write(s.yy); fw.Write(s.ii);
+                    }
+
+                    // === Вычисление координат следующего вложенного шестиугольника ===
+                    double[] nextX = new double[n];
+                    double[] nextY = new double[n];
+
+                    for (int i = 0; i < n; i++)
+                    {
+                        int nextIdx = (i + 1) % n;
+                        // Новая точка лежит на отрезке между текущей и следующей вершиной
+                        nextX[i] = x[i] + p * (x[nextIdx] - x[i]);
+                        nextY[i] = y[i] + p * (y[nextIdx] - y[i]);
+                    }
+
+                    // Обновляем массивы координат для следующей итерации
+                    Array.Copy(nextX, x, n);
+                    Array.Copy(nextY, y, n);
+                }
+            }
+            MessageBox.Show("Сгенерировано 20 вложенных шестиугольников в файл SCRATCH!", "Успех");
+        }
+
+        private void b_read_from_file_Click(object sender, EventArgs e)
+        {
+            // Сюда ты вставляешь код преподавателя (Два прохода: первый для Xmin/Xmax, второй для рисования)
+            // Ниже приведен каркас алгоритма из методички:
+
+            if (!File.Exists("SCRATCH"))
+            {
+                MessageBox.Show("Сначала сгенерируйте файл!");
+                return;
+            }
+
+            Graphics dc = pictureBox1.CreateGraphics();
+            Pen pen = new Pen(Brushes.Blue, 1);
+            Simple s;
+
+            double xmin = double.MaxValue, xmax = double.MinValue;
+            double ymin = double.MaxValue, ymax = double.MinValue;
+
+            // ПЕРВЫЙ ПРОХОД: Поиск минимальных и максимальных координат
+            using (BinaryReader fr = new BinaryReader(File.Open("SCRATCH", FileMode.Open)))
+            {
+                while (fr.BaseStream.Position < fr.BaseStream.Length)
+                {
+                    double x = fr.ReadDouble();
+                    double y = fr.ReadDouble();
+                    int code = fr.ReadInt32();
+
+                    if (x < xmin) xmin = x;
+                    if (x > xmax) xmax = x;
+                    if (y < ymin) ymin = y;
+                    if (y > ymax) ymax = y;
+                }
+            }
+
+            // Расчет коэффициентов масштабирования (по методичке)
+            double fx = (Xmax - Xmin) / (xmax - xmin);
+            double fy = (Ymax - Ymin) / (ymax - ymin);
+            double f = (fx < fy) ? fx : fy;
+
+            double xC = 0.5 * (xmin + xmax);
+            double yC = 0.5 * (ymin + ymax);
+            double XC = 0.5 * (Xmin + Xmax);
+            double YC = 0.5 * (Ymin + Ymax);
+
+            double c1 = XC - f * xC;
+            double c2 = YC - f * yC;
+
+            // ВТОРОЙ ПРОХОД: Отрисовка
+            int Xold = 0, Yold = 0;
+            using (BinaryReader fr = new BinaryReader(File.Open("SCRATCH", FileMode.Open)))
+            {
+                while (fr.BaseStream.Position < fr.BaseStream.Length)
+                {
+                    double x = fr.ReadDouble();
+                    double y = fr.ReadDouble();
+                    int code = fr.ReadInt32();
+
+                    // Перевод в экранные координаты
+                    double calcX = f * x + c1;
+                    double calcY = f * y + c2;
+
+                    int screenX = IX(calcX);
+                    int screenY = IY(calcY);
+
+                    if (code == 1)
+                    {
+                        dc.DrawLine(pen, Xold, Yold, screenX, screenY);
+                    }
+                    Xold = screenX;
+                    Yold = screenY;
+                }
+            }
+        }
+
+        private int IX(double x)
+        {
+            return (int)(x * (pictureBox1.Size.Width / 10.0) + 0.5); // 10.0 - условная ширина окна из методички
+        }
+
+        private int IY(double y)
+        {
+            return (int)(pictureBox1.Size.Height - y * (pictureBox1.Size.Height / 7.0) + 0.5); // 7.0 - условная высота
+        }
+
+        private void b_clear_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Refresh(); // Очищает PictureBox
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void b_exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void b_change_output_size_Click(object sender, EventArgs e)
+        {
+            // Изменяем область вывода согласно введенным в TextBox значениям
+            try
+            {
+                Xmin = Convert.ToDouble(tb_x1.Text);
+                Ymin = Convert.ToDouble(tb_y1.Text);
+                Xmax = Convert.ToDouble(tb_x2.Text);
+                Ymax = Convert.ToDouble(tb_y2.Text);
+                MessageBox.Show("Координаты области вывода изменены.");
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка! Введите корректные числовые значения.");
+            }
+        }
+    }
+}
